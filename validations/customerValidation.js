67adbaf3bf2de8950bf35b0fad4580697ad0ebed
Aapna-village <gouravsh7568@gmail.com>
2024-07-19 12:06:20 +0530

    wew

--- validations/customerValidation.js
+++ validations/customerValidation.js
@@ -4,6 +4,7 @@ const customerValidation = [
   body("firstName").notEmpty().withMessage("First name is required"),
   body("lastName").notEmpty().withMessage("Last name is required"),
   body("email").isEmail().withMessage("Valid email is required"),
+  happybirthday,
 ];
 
 module.exports = { customerValidation };
